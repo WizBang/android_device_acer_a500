848fab9880ba8f88c4ed3a3360040d8858dcba42
Anthony Papousek <anthony@papousek.org>
2013-08-23 22:46:14 -0500

    Added bugmailer config

--- a500.mk
+++ a500.mk
@@ -146,6 +146,12 @@ PRODUCT_CHARACTERISTICS := tablet
 PRODUCT_COPY_FILES += \
     system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf \
 
+# Bugmailer config
+PRODUCT_PACKAGES += send_bug
+PRODUCT_COPY_FILES += \
+    system/extras/bugmailer/bugmailer.sh:system/bin/bugmailer.sh \
+    system/extras/bugmailer/send_bug:system/bin/send_bug
+
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
 
